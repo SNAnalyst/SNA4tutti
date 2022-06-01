61ef0785df991ff71f14830c7e093a9843a35fa1
Zucca <c.zucca@jads.nl>
2022-06-01 14:54:27 +0200

    attempted test setup in tinytest. general and tut 1

---
+++ tests/tinytest/test-sna4tutti-tutorials.R
@@ -0,0 +1,12 @@
+library(shinytest)
+
+  # Don't run these tests on the CRAN build servers
+  # skip_on_cran() ADAPT TO TINYTEST
+
+  # Use compareImages=FALSE because the expected image screenshots were created
+  # on a Mac, and they will differ from screenshots taken on the CI platform,
+  # which runs on Linux.
+
+  appdir <- system.file("tutorials/lab01_IntroR", package = "sna4tutti")
+  # expect_pass(testApp(appdir, compareImages = FALSE)) ADAPT TO TINYTEST
+
