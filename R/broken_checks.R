81f83c882bf72c6dc725f83738d9e05ce68a6fb4
Zucca <c.zucca@jads.nl>
2022-06-16 13:32:43 +0200

    function broken_info up and running CAREFUL: 2 tutorial 5 are up now

---
+++ R/broken_checks.R
@@ -0,0 +1,20 @@
+#' Within tutorial sna4tutti unit test error report function
+#'
+#'
+#' This function prints an error message in case of broken code chunk and it
+#' reports the name of the broken code box
+#'
+#' @return nothing relevant, this function is useful for its side effect.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' broken_info()
+#' }
+broken_info <- function(broken_list = list() ) {
+  lab <- knitr::opts_current$get(name = "label")
+  broken_list <<- append(broken_list, lab)
+  cat("=====================================================================\n")
+  cat(">>> Codebox \'", lab , "\' is broken, please contact the maintainers! <<<\n")
+  cat("=====================================================================\n")
+}
