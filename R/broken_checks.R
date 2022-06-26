1d54735de7fdc1474e43d9c3db516c28c5dfb1d9
Zucca <c.zucca@jads.nl>
2022-06-26 17:42:03 +0200

    tests tut 5 up + small changes error storage and function broken_checks

--- R/broken_checks.R
+++ R/broken_checks.R
@@ -11,11 +11,11 @@
 #' \dontrun{
 #' broken_info()
 #' }
-broken_info <- function(broken_list = list() ) {
+broken_info <- function(broken_list = l ) {
+  l <- list()
   lab <- knitr::opts_current$get(name = "label")
   broken_list <<- append(broken_list, lab)
-  cat("=========================================\n")
-  cat(">>> Codebox \'", lab , "\' is broken \n")
-  cat("please contact the maintainers! <<<\n")
-  cat("=========================================\n")
+  cat("==========================================================================\n")
+  cat(">>> Codebox \'", lab , "\' is broken. Please, contact the maintainers! <<<\n")
+  cat("==========================================================================")
 }
