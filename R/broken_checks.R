8ae9765f9f372af63e64910658f2c6a1bb242533
Zucca <c.zucca@jads.nl>
2022-07-01 12:07:02 +0200

    updates with tutorial8 and RCMD checks run on the entire environment

--- R/broken_checks.R
+++ R/broken_checks.R
@@ -3,14 +3,18 @@
 #'
 #' This function prints an error message in case of broken code chunk and it
 #' reports the name of the broken code box
+#' @param broken_list internal parameter to fetch the name of the code box with broken code
 #'
 #' @return nothing relevant, this function is useful for its side effect.
-#' @export
+#'
 #'
 #' @examples
 #' \dontrun{
 #' broken_info()
 #' }
+#'
+#'
+#' @export
 broken_info <- function(broken_list = l ) {
   l <- list()
   lab <- knitr::opts_current$get(name = "label")
